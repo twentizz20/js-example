84afd61e77caccf0359eb3d78b496a479f2324d3
yjlee <twentizz20@gmail.com>
2023-05-05 22:18:25 +0900

    계산기 2 사칙연산 기능 추가 230505 계산기2 +-*/ 기능 추가

--- js/calc.js
+++ js/calc.js
@@ -35,3 +35,33 @@ basic2ResultPlus.addEventListener("click", () => {
 
   basic2ResultContent.textContent = result;
 });
+
+const basic2ResultMinus = document.querySelector(".basic2-result-minus");
+basic2ResultMinus.addEventListener("click", () => {
+  const result = Number(basic2Input1.value) - Number(basic2Input2.value);
+
+  basic2Input1.value = "";
+  basic2Input2.value = "";
+
+  basic2ResultContent.textContent = result;
+});
+
+const basic2ResultMultiply = document.querySelector(".basic2-result-multiply");
+basic2ResultMultiply.addEventListener("click", () => {
+  const result = Number(basic2Input1.value) * Number(basic2Input2.value);
+
+  basic2Input1.value = "";
+  basic2Input2.value = "";
+
+  basic2ResultContent.textContent = result;
+});
+
+const basic2ResultDivide = document.querySelector(".basic2-result-divide");
+basic2ResultDivide.addEventListener("click", () => {
+  const result = Number(basic2Input1.value) / Number(basic2Input2.value);
+
+  basic2Input1.value = "";
+  basic2Input2.value = "";
+
+  basic2ResultContent.textContent = result;
+});
